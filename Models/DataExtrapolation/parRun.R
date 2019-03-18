6d88d57ec34791c4a8bcc745fdb199b7c6cf3a50
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-03-18 15:04:04 +0100

    formatted_extrapolated_data_communes

--- Models/DataExtrapolation/parRun.R
+++ Models/DataExtrapolation/parRun.R
@@ -8,6 +8,8 @@ years=c('07','08','09','10','11')
 
 csp = c("EMP","OUV","INT","ART","CAD")
 
+idcol = 'COM'
+
 library(doParallel)
 cl <- makeCluster(50,outfile='log')
 registerDoParallel(cl)
@@ -15,8 +17,6 @@ registerDoParallel(cl)
 for(year in years){
   
   show(paste0("Estimation for year 20",year))
-  
-  idcol = 'COM'
   
   income <- getIncome(year,idcol=idcol)
   structure <-getStructure(year,idcol=idcol)
@@ -47,25 +47,59 @@ stopCluster(cl)
 ##
 #load('res/est_gaussian_2011.RData')
 
+# year = '01'
+# load(paste0('res/extr_20',year,'_',idcol,'.RData'))
+# sapply(estimations,length)
+# estimations[[5]]
+# test=list();for(k in 1:6){test[[k]]=estimations[[k]]}
+# testc=list();for(k in 1:6){testc=append(testc,flatten(estimations[[k]]))}
+
+
+
 fullres=data.frame()
 for(year in years){
+  show(year)
   load(paste0('res/extr_20',year,'_',idcol,'.RData'))
-  fullres = rbind(fullres,matrix(sapply(estimations,function(l){
-    c(l$medincome,
-      l$avincome,
-      l$stdincome,
-      l$shares,
-      l$distrib,
-      l$gaussianvalmax,
-      l$lognormalvalmax
+  firstind = which(sapply(estimations,length)>8)[1]
+  cols = length(estimations[[firstind]]$medincome)*4+4
+  currentdata = sapply(estimations,function(l){
+    #show(length(l))
+    res = rep(NA,cols)
+    if(length(l)>1){
+      res = c(l$id,
+              l$medincome,
+              l$avincome,
+              l$stdincome,
+              l$shares,
+              l$distrib,
+              l$gaussianvalmax,
+              l$lognormalvalmax
       )
-    }),ncol=length(estimations[[1]]$medincome)*4+3,
+    }
+    return(res)
+  }
+                       
+                       )
+  
+  fullres = rbind(fullres,matrix(
+    currentdata,ncol=cols,
     byrow = T)
   )
 }
-names(fullres)<-c(paste0('medIncome',csp),paste0('avgIncome',csp),paste0('stdIncome',csp),paste0('share',csp),
+names(fullres)<-c("id",paste0('medIncome_',csp),paste0('avgIncome_',csp),paste0('stdIncome_',csp),paste0('share_',csp),
                   'distribution','optgaussian','optlognormal'
                   )
+for(j in (1:ncol(fullres))[c(-1,-22)]){fullres[,j]<-as.numeric(as.character(fullres[,j]))}
+as.tbl(fullres)
+
+# table(as.character(sapply(estimations,length)))
+# for year 2001 : more than 1/2 with no data !
+#1  13   8 
+#1 528 771 
+
+# filter 0 and NAs
+#length(which(nchar(as.character(fullres$id))>0))
+fullres=fullres[which(nchar(as.character(fullres$id))>0),]
 
 write.table(fullres,file='res/extrapolate_allyears_communes.csv',sep = ';',row.names = F,col.names = T)
 
