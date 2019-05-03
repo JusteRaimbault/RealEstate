a960ffccea63f58da92526d29d09e0c7d3e26477
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-05-03 21:23:48 +0200

    .

--- Models/Consolidation/consolidation_final.R
+++ Models/Consolidation/consolidation_final.R
@@ -48,13 +48,13 @@ for(csp in unique(bien$CSP)){
 
 # TODO
 #bien$medianRevdispo = ...
-
+bien$medianRevdispo = rep(50000,nrow(bien))
 
 
 #######
 # 2) load the models and apply them to the reconstructed database
 
-patrimoine_models = as.tbl(read.csv('models.csv',sep=';')) 
+patrimoine_models = read.csv('models.csv',sep=';')
 
 # correspondance between var names
 
@@ -62,9 +62,44 @@ consolidated = bien[,c('DepCom','Age_acq','REQ_PRIX','CSP','medianZnivvie','medi
 names(consolidated) <- c('IDCOM','AGE','PRIX','CSP','medianZnivvie','medianRevdispo')
 
 # create dummies for discrete var
+for(csp in unique(consolidated$CSP)){
+  consolidated[[paste0('CSP_',csp)]] = as.numeric(consolidated$CSP==csp)
+}
 
+explicative = c('AGE','PRIX',paste0('CSP_',unique(consolidated$CSP)),'medianZnivvie','medianRevdispo')
 
+model_names = c('AGEPR'='AGE','RESPRIACH'='PRIX','CSPRprof_inter'='CSP_INT','CSPRprof_inter.1'='CSP_ART','CSPRouvriers'='CSP_OUV',
+                'CSPRemployes'='CSP_EMP','CSPRCPIS'='CSP_CAD','ZNIVVIE'='medianZnivvie','Rev_Disp'='medianRevdispo')
+models_coefs = patrimoine_models[,c('AGEPR','RESPRIACH','CSPRprof_inter','CSPRprof_inter','CSPRouvriers','CSPRemployes','CSPRCPIS','ZNIVVIE','Rev_Disp')]
+names(models_coefs)<- model_names[names(models_coefs)]
+models_coefs[is.na(models_coefs)]=0
+rownames(models_coefs)<-paste0(as.character(patrimoine_models$explainedvar),'_',as.character(patrimoine_models$type),'_',as.character(patrimoine_models$with_respriach))
 
+m = data.frame(consolidated[,explicative])
+for(j in 1:ncol(m)){m[,j]=as.numeric(as.character(m[,j]))}
+#m[['medianZnivvie']]=0;m[['medianRevdispo']]=0
+m = as.matrix(m)
+m = m[,explicative]
+coefs = t(as.matrix(models_coefs))
+coefs = coefs[explicative,]
+
+final_consolidated = m%*%coefs
+summary(final_consolidated)
+
+# ?
+# - check filterings
+
+res = data.frame(final_consolidated[,paste0(unique(as.character(patrimoine_models$explainedvar)),'_BIEN_TRUE')])
+summary(res)
+
+#hist(res$MONTCLA_BIEN_TRUE,breaks=100)
+# TODO : check distribs ; reasonable depending on explicative variables distributions ?
+# OR compare to hist of patrimoine => should have fitted distributions ! - compare summaries
+
+removeOutliers<-function(x){x[x>quantile(x,c(0.05),na.rm=T)&x<quantile(x,c(0.95),na.rm=T)]}
+
+hist(removeOutliers(res$MONTCLA_BIEN_TRUE),breaks=100)
+# => too small values in nivvie or rev_disp ?
 
 
 
